ace049097920367e50a518c263e876b5dac5b787
Grant Beard <Grantxbeard@gmail.com>
2022-12-12 22:03:43 -0700

    add display

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Display from '../Display/Display';
 
 const App = () => {
   return (
---
+++ src/Components/Display/Display.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './Display.css';
+
+const Display = () => {
+    return (
+        <h2>Display</h2>
+    )
+}
+
+export default Display
