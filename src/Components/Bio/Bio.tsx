eb891e629bf222c60f4c02b6c0871b63bd0be61a
Grant Beard <Grantxbeard@gmail.com>
2022-12-14 20:22:13 -0700

    structure bio page

--- src/Components/Bio/Bio.tsx
+++ src/Components/Bio/Bio.tsx
@@ -3,7 +3,18 @@ import './Bio.css'
 
 const Bio = () => {
     return (
-        <div>Bio</div>
+        <div className="bio-wrapper">
+            <div className="bio-box">
+                <img className='portrait' src='https://i.ibb.co/Y83Vcjy/IMG-3311.jpg'/>
+                <div className='vl'></div>
+                <div>
+                    <p className='copy'>Grant Beard is a multidisciplinary artist living and working in the Denver area.</p>
+                    <a href='https://www.instagram.com/grantxbeard/'>
+                            <img className='insta-logo' src='https://i.ibb.co/YkVmf9V/instagram-icon-white-on-black-circle.jpg'/>
+                    </a>
+                </div>
+            </div>
+        </div>
     )
 }
 
