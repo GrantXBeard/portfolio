79bb4351314a01d85f4923b7cf44887480044045
Grant Beard <Grantxbeard@gmail.com>
2022-12-15 16:47:48 -0700

    add overlays

--- src/Components/Image/Image.tsx
+++ src/Components/Image/Image.tsx
@@ -5,15 +5,20 @@ import Modal from '../Modal/Modal'
 interface IImageProps {
     source: string
     description: string
+    color: string
 }
 
-const Image = ({ source, description }: IImageProps) => {
+const Image = ({ source, description, color }: IImageProps) => {
 
     const [showModal, setShowModal] = useState(false)
 
     return (
         <>
-        <img onClick={() => setShowModal(true)} className="thumbnail" src={source}/>
+        <div className="img-wrapper">
+            <div className={`overlay-${color}`} >
+                <img onClick={() => setShowModal(true)} className="thumbnail opacity" src={source}/>
+            </div>
+        </div>
         {showModal && <Modal setShow={setShowModal} source={source} description={description} />}
         </>
     )
